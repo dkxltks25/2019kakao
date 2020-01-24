97be9903244af1620939a8f0a440adceff55293e
bagjaehong <dkxltks25@naver.com>
2020-01-24 23:03:53 +0900

    test

--- kakao2.js
+++ kakao2.js
@@ -2,9 +2,29 @@
 
 function solution(N,stages){
     let answer = [];
-    
+    const AllInfo = {};
+    let befor_answer = [];
+    stages.map((index)=>{
+        AllInfo[index] === undefined ? AllInfo[index] = 1 : AllInfo[index]++;
+    })
+    let NowNum = 1;
+    let Count = stages.length;
+    for (let [key, value] of Object.entries(AllInfo)) {
+        if(NowNum !== key){
+            for(let NowNum; key < NowNum; NowNum++){
+                befor_answer.push([keys,0]);
+            }
+        }
+        console.log(Count,value);
+
+        befor_answer.push([key,value/Count]);
+        Count = Count - value;
+        NowNum++;
+      }
+    console.log(befor_answer)
     return answer;
 
 }
 
 solution(5,[2, 1, 2, 6, 2, 4, 3, 3]);
+solution(4,[4,4,4,4,4])
