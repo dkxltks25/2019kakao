d7048b7fd127507c458091cdf0be4f5dda01c421
bagjaehong <dkxltks25@naver.com>
2020-01-29 02:24:33 +0900

    효율성 문제 미해결

--- kakao4.js
+++ kakao4.js
@@ -1,89 +1,36 @@
-//음식이 비어있을경우 0 비어 있지 않을경우 -1을 하고 반환해준다.
-function findFood(food) {
-    let Able = true;
-    let MinusCount = 0;
-    food = food.map(index => {
-        if (index !== 0) {
-            MinusCount++;
-            return index - 1;
-        } else {
-            return index;
-        }
-    });
-    if (MinusCount === 0) {
-        Able = false;
-        return [Able, 0, food];
-    } else {
-        return [Able, MinusCount, food];
-    }
+function solution(food_items,k){
+    
+    const food = food_items.map((index,number)=>{
+        return [number+1,index]
+    })    
+    return MuziEat(food,k);
+    
 }
-function EatOrFind(food, time) {
-    let Able = true;
-    let Count = 0;
-    food.map(index => {
-        if (index !== 0) {
-            Count++;
+function MuziEat(food,k){
+    let p = 0;
+    for( ; k >0;k--){
+        if(food.length === 0){
+            return -1;
         }
-    });
-    if (Count >= time) {
-        Able = true;
-    } else {
-        Able = false;
-    }
-    return Able;
-}
-function foodPosition(food, time) {
-    let Able = true;
-    let Count = 0;
-    let ReturnValue = -1;
-    food.map((index, number) => {
-        if (Able) {
-            if(number === 0){
-                if(index !== 0){
-                    ReturnValue = number+1;
-                }else{
-                    ReturnValue = -1;
-                }
-            }
-            else if(time === 0){
-                if(index !== 0){
-                    ReturnValue = number+1;
-                    Able = false;
-                }
-            }else{
-                if(index !== 0){
-                    Count++; 
-                    time--;
-                } 
-            }
+        if(p === food.length){
+            p = 0;
         }
-    });
-
-    return ReturnValue;
-}
-function solution(food_times, k) {
-    var answer = 0;
-    while (true) {
-        const State = EatOrFind(food_times, k);
-        // console.log(State);
-        //음식을 먹아야하는 겨우와 찾아야하는 음식의 경우 find true Eat false
-        if (!State) {
-            let EatState = true;
-            let Count = 0;
-            [EatState, Count, food_times] = findFood(food_times);
-            if (EatState) {
-                k -= Count;
-            } else {
-                return -1;
-            }
-        } else {
-            let t = foodPosition(food_times, k);
-            //console.log(t);
-            return t;
+        food[p][1] -= 1;
+        if(food[p][1] ===  0){
+            food.splice(p,1);
+        }else{
+            p++;
         }
     }
-    return answer;
+    if(food.length === 0){
+        return -1;
+    }
+    if(p === food.length){
+        p = 0;
+    }
+    return food[p][0]
 }
 //solution([3, 1, 2], 5);
 //solution([3,1,1,1,2,4,3],12)
 //solution([4, 3, 5, 6, 2],7)
+solution([5,5,5],15)
