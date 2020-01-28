a726c9e1a7216c02a7f82cb77df35aedc35c9b91
bagjaehong <dkxltks25@naver.com>
2020-01-29 01:42:57 +0900

    kakao4 problem ...ing

--- kakao4.js
+++ kakao4.js
@@ -1,69 +1,89 @@
-        let Answer = 0;
-        function solution(food_times, k) {
-            let answer = 0;
-            MuziEat(food_times,k,0,0)
-            console.log(Answer);
-            console.log(1)
-            return answer+1;
-            
+//음식이 비어있을경우 0 비어 있지 않을경우 -1을 하고 반환해준다.
+function findFood(food) {
+    let Able = true;
+    let MinusCount = 0;
+    food = food.map(index => {
+        if (index !== 0) {
+            MinusCount++;
+            return index - 1;
+        } else {
+            return index;
         }
-        let MuziEatStop = false;
-        function MuziEat(_food,time,now,empty){
-            console.log(_food,time,now,empty)
-            //재귀함수 종료
-            if(MuziEatStop){
-                return ;
-            } 
-            //범위 초과
-            if(now === _food.length){
-                now = 0;
-                empty = 0;
-                MuziEat(_food,time,now,empty);
-            }
-            //다먹음
-            else if(empty === _food.length){
-                MuziEatStop = true;
-                Answer = -1;
-                return ;
-            }
-            //시간 초과
-            else if(time === 0){
-                if(_food[now]=== 0){
-                    for(let i = now; i< _food.length;i++){
-                        if(i !== _food.length-1){
-                            if(_food[i] !== 0){
-                                Answer = i;
-                                i = _food.length;
-                            }
-                        }else{
-                            if(_food[i] !== 0){
-                                Answer = i;
-                                i = _food.length
-                            }else{
-                                i = 0;
-                            }
-                        }
-                    }
+    });
+    if (MinusCount === 0) {
+        Able = false;
+        return [Able, 0, food];
+    } else {
+        return [Able, MinusCount, food];
+    }
+}
+function EatOrFind(food, time) {
+    let Able = true;
+    let Count = 0;
+    food.map(index => {
+        if (index !== 0) {
+            Count++;
+        }
+    });
+    if (Count >= time) {
+        Able = true;
+    } else {
+        Able = false;
+    }
+    return Able;
+}
+function foodPosition(food, time) {
+    let Able = true;
+    let Count = 0;
+    let ReturnValue = -1;
+    food.map((index, number) => {
+        if (Able) {
+            if(number === 0){
+                if(index !== 0){
+                    ReturnValue = number+1;
                 }else{
-                    Answer = now;
+                    ReturnValue = -1;
                 }
-                MuziEatStop = true;
             }
-            //음식 섭취
-            else if(_food[now] !== 0){
-                _food[now]--;
-                MuziEat(_food,time-1,now+1,empty);
+            else if(time === 0){
+                if(index !== 0){
+                    ReturnValue = number+1;
+                    Able = false;
+                }
             }else{
-                MuziEat(_food,time,now+1,empty+1);
-            }   
+                if(index !== 0){
+                    Count++; 
+                    time--;
+                } 
+            }
         }
+    });
 
-/*
-    배열의 값
-*/
-// 시작부터 끝까지의 계수를 전부 -1
-// 근대 이게 가능한거는 갯수가 
-
+    return ReturnValue;
+}
+function solution(food_times, k) {
+    var answer = 0;
+    while (true) {
+        const State = EatOrFind(food_times, k);
+        // console.log(State);
+        //음식을 먹아야하는 겨우와 찾아야하는 음식의 경우 find true Eat false
+        if (!State) {
+            let EatState = true;
+            let Count = 0;
+            [EatState, Count, food_times] = findFood(food_times);
+            if (EatState) {
+                k -= Count;
+            } else {
+                return -1;
+            }
+        } else {
+            let t = foodPosition(food_times, k);
+            //console.log(t);
+            return t;
+        }
+    }
+    return answer;
+}
+//solution([3, 1, 2], 5);
 //solution([3,1,1,1,2,4,3],12)
-solution([3,1,2],6);
-
+//solution([4, 3, 5, 6, 2],7)
